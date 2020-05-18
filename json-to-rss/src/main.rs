60a4483fdc1efe0d3c86d70404f51ad705078910
lupyuen <luppy@appkaki.com>
2020-05-18 16:01:34 +0800

    Adding RSS

--- json-to-rss/src/main.rs
+++ json-to-rss/src/main.rs
@@ -1,24 +1,18 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize};
 use rss::ChannelBuilder;
 use rss::Channel;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 struct Point {
     x: i32,
     y: i32,
 }
 
 fn main() {
-    let point = Point { x: 1, y: 2 };
+    let json = std::fs::read_to_string("../resume.json").unwrap();
 
-    // Convert the Point to a JSON string.
-    let serialized = serde_json::to_string(&point).unwrap();
-
-    // Prints serialized = {"x":1,"y":2}
-    println!("serialized = {}", serialized);
-
-    // Convert the JSON string back to a Point.
-    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+    // Convert the JSON string to a Point.
+    let deserialized: Point = serde_json::from_str(&json).unwrap();
 
     // Prints deserialized = Point { x: 1, y: 2 }
     println!("deserialized = {:?}", deserialized);
