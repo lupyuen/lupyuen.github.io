21cd6193df81b88c0c8733dbd92c658726fa0ddb
lupyuen <luppy@appkaki.com>
2020-05-18 16:36:05 +0800

    Adding RSS

--- json-to-rss/src/main.rs
+++ json-to-rss/src/main.rs
@@ -1,6 +1,5 @@
-use serde::{Deserialize};
-use rss::ChannelBuilder;
-//  use rss::Channel;
+use serde::{ Deserialize };
+use rss::{ ChannelBuilder, ItemBuilder };
 
 /*
 "publications": [
@@ -14,8 +13,8 @@ use rss::ChannelBuilder;
 */
 
 #[derive(Deserialize, Debug)]
-struct Point {
-    publications: [Publication; 30],
+struct Resume {
+    publications: Vec<Publication>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -25,25 +24,50 @@ struct Publication {
     publisher: String,      //  "OpenGov",
     releaseDate: String,    // "2018-03-20",
     website: String,        // "https://www.opengovasia.com/articles/developing-cost-effective-energy-efficient-iot-solutions-for-outdoor-as-well-as-indoor-applications",
-    //  summary: String,        // "Lup Yuen talks about two classes of IoT, ‘deep’ IoT and ‘wide’ IoT. Deep IoT devices require high bandwidth and power supply. UnaBiz looks at wide IoT, which refers to devices that are very light, battery-powered and operate on pervasive networks. They can work anytime, anywhere in Singapore and do not rely on WiFi or the cellular network."
+    #[serde(default)]
+    summary: String,        // "Lup Yuen talks about two classes of IoT, ‘deep’ IoT and ‘wide’ IoT. Deep IoT devices require high bandwidth and power supply. UnaBiz looks at wide IoT, which refers to devices that are very light, battery-powered and operate on pervasive networks. They can work anytime, anywhere in Singapore and do not rely on WiFi or the cellular network."
 }
 
 fn main() {
     let json = std::fs::read_to_string("../resume.json").unwrap();
 
     // Convert the JSON string to a Point.
-    let deserialized: Point = serde_json::from_str(&json).unwrap();
+    let deserialized: Resume = serde_json::from_str(&json).unwrap();
 
     // Prints deserialized = Point { x: 1, y: 2 }
-    println!("deserialized = {:?}", deserialized);
+    //  println!("deserialized = {:?}", deserialized);
+
+    let mut items = Vec::new();
+
+    /*
+    <item>
+    <title>Example entry</title>
+    <description>Here is some text containing an interesting description.</description>
+    <link>http://www.example.com/blog/post/1</link>
+    <guid isPermaLink="false">7bd204c6-1655-4c27-aeee-53f933c5395f</guid>
+    <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
+    </item>
+    */
+    let item = ItemBuilder::default()
+        .title(Some("aaa".to_string()))
+        .description(Some("bbb".to_string()))
+        .link(Some("ccc".to_string()))
+        .pub_date(Some("ddd".to_string()))
+        .build()
+        .unwrap();
+    items.push(item);
 
     let channel = ChannelBuilder::default()
-    .title("Channel Title")
-    .link("http://example.com")
-    .description("An RSS feed.")
-    .build()
-    .unwrap();
+        .title("lupyuen")
+        .link("https://lupyuen.github.io")
+        .description("IoT Techie and Educator")
+        .items(items)
+        .build()
+        .unwrap();
+
+    let string = channel.to_string(); // convert the channel to a string
+    println!("{:?}", string);
+
     //  let channel = Channel::default();
-    channel.write_to(::std::io::sink()).unwrap(); // // write to the channel to a writer
-    //  let string = channel.to_string(); // convert the channel to a string
+    //  channel.write_to(::std::io::sink()).unwrap(); // // write to the channel to a writer
 }
