753c984e10b3b0ba5bde29784be003429745f738
lupyuen <luppy@appkaki.com>
2021-01-08 09:50:06 +0800

    Generate all articles

--- scripts/gen-articles.sh
+++ scripts/gen-articles.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env bash
 # Convert articles from Markdown to HTML
 
-# Article to be generated
-article=led
-
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
-# Generate the article header
-cat scripts/articles/$article-header.html \
-    scripts/rustdoc-header.html \
-    >article-rustdoc-header.html
+# Generate article
+function generate_article() {
+    local article=$1
 
-# Convert the article with rustdoc
-rustdoc \
-    --output articles \
-    --html-in-header article-rustdoc-header.html \
-    --html-before-content \
-    scripts/rustdoc-before.html \
-    src/$article.md
+    # Generate the article header
+    cat scripts/articles/$article-header.html \
+        scripts/rustdoc-header.html \
+        >article-rustdoc-header.html
 
-# Delete the article header
-rm article-rustdoc-header.html
+    # Convert the article with rustdoc
+    rustdoc \
+        --output articles \
+        --html-in-header article-rustdoc-header.html \
+        --html-before-content \
+        scripts/rustdoc-before.html \
+        src/$article.md
+
+    # Delete the article header
+    rm article-rustdoc-header.html
+}
+
+# Generate an article
+# generate_article led
+
+# Generate all articles in src
+for f in src/*.md
+do
+    echo $f
+    filename=$(basename -- "$f")
+    # extension="${filename##*.}"
+    filename="${filename%.*}"
+    generate_article $filename
+done
