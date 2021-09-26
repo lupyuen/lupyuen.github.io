5e145b5adf71305fa316c20b3a9b71329e374700
Lee Lup Yuen <luppy@appkaki.com>
2021-09-26 17:52:48 +0800

    Test code highlighting for Rust article

--- scripts/gen-articles.sh
+++ scripts/gen-articles.sh
@@ -35,18 +35,25 @@ function generate_article() {
     #   <code><code>...</code></code>
     # To...
     #   <code>...</code>
+    # TODO: Fix this hack for removing leading blank line in Rust code blocks
+    # Change...
+    #   <pre class="rust rust-example-rendered"><code>
+    # To...
+    #   <pre class="rust rust-example-rendered"><code style="position: relative; top: -15px">
     cp  $html $tmp
     cat $tmp \
-        | sed 's/<pre class="\(.*\)">/<pre class="\1"><code>/' \
+        | sed 's/\(<pre[^>]*>\)/\1<code>/' \
         | sed 's/<\/pre>/<\/code><\/pre>/' \
         | sed 's/<code><code>/<code>/' \
         | sed 's/<\/code><\/code>/<\/code>/' \
+        | sed 's/<pre class="rust rust-example-rendered"><code>/<pre class="rust rust-example-rendered"><code style="position: relative; top: -15px">/' \
         >$html
     rm $tmp
 }
 
 # Generate an article
-# generate_article rust
+generate_article rust
+exit
 
 # Generate all articles in src
 for f in src/*.md
