92238673224e5784320e2ab7f6fb8d271b6b5f91
Lee Lup Yuen <luppy@appkaki.com>
2023-05-18 15:42:58 +0800

    Sync from lupyuen.github.io to lupyuen.codeberg.pages

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,47 +4,21 @@
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
-# Generate article
+# Rewrite lupyuen.github.io to lupyuen.codeberg.page in articles/$1.html
 function generate_article() {
     local article=$1
     local html=articles/$article.html
     local tmp=$article.tmp
 
-    # Generate the article header
-    cat scripts/articles/$article-header.html \
-        scripts/rustdoc-header.html \
-        >article-rustdoc-header.html
-
-    # Convert the article with rustdoc
-    rustdoc \
-        --output articles \
-        --html-in-header article-rustdoc-header.html \
-        --html-before-content \
-        scripts/rustdoc-before.html \
-        src/$article.md
-
-    # Delete the article header
-    rm article-rustdoc-header.html
-
-    # Fix the rustdoc output to work with Prism.js code highlighting
-    # Change...
-    #   <pre class="...">...</pre>
-    # To...
-    #   <pre class="..."><code>...</code></pre>
-    # Change...
-    #   <code><code>...</code></code>
-    # To...
-    #   <code>...</code>
     cp  $html $tmp
     cat $tmp \
-        | sed 's/\(<pre[^>]*>\)/\1<code>/' \
-        | sed 's/<\/pre>/<\/code><\/pre>/' \
-        | sed 's/<code><code>/<code>/' \
-        | sed 's/<\/code><\/code>/<\/code>/' \
+        | sed 's/lupyuen.github.io\/images/lupyuen.codeberg.page\/images/' \
+        | sed 's/lupyuen.github.io\/articles/lupyuen.codeberg.page\/articles/' \
         >$html
     rm $tmp
 }
 
+# Update the current folder
 git pull
 
 # Sync to this folder
@@ -63,26 +37,29 @@ cp scripts/articles/* $sync/scripts/articles/
 cp src/* $sync/src/
 set -x  #  Echo all commands.
 
-# TODO: Rewrite lupyuen.github.io to lupyuen.codeberg.page in all HTML and MD files
+# Go to the sync folder
+pushd $sync
+
+# Testing: Rewrite lupyuen.github.io to lupyuen.codeberg.page
+# generate_article lte2 ; exit
+
+# Rewrite lupyuen.github.io to lupyuen.codeberg.page in articles/*.html
+set +x  #  Disable Echo.
+for f in src/*.md
+do
+    # echo $f
+    filename=$(basename -- "$f")
+    # extension="${filename##*.}"
+    filename="${filename%.*}"
+    generate_article $filename
+done
+set -x  #  Echo all commands.
 
 # Commit the modified files
-pushd $sync
 git status
 git add .
 git commit --all --message="Sync from lupyuen.github.io"
 git push
+
+# Return to the previous folder
 popd
-
-# Generate an article
-# generate_article rust ; exit
-
-# Generate all articles in src
-# for f in src/*.md
-# do
-#     echo $f
-#     filename=$(basename -- "$f")
-#     # extension="${filename##*.}"
-#     filename="${filename%.*}"
-#     generate_article $filename
-# done
-
