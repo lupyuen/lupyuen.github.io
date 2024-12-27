22c5fd6a4f704ca2d8ce80b188e90ea79362acae
Lup Yuen Lee <luppy@appkaki.com>
2024-12-28 06:44:47 +0800

    Sync to lupyuen.org

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,22 +4,36 @@
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
-# Rewrite lupyuen.github.io to lupyuen.codeberg.page in articles/$1.html
+# Rewrite lupyuen.github.io to lupyuen.org or lupyuen.codeberg.page in articles/$1.html
 function generate_article() {
     local article=$1
     local html=articles/$article.html
     local tmp=$article.tmp
 
     cp  $html $tmp
-    cat $tmp \
-        | sed 's/lupyuen.github.io\"/lupyuen.codeberg.page\"/g' \
-        | sed 's/lupyuen.github.io\/articles/lupyuen.codeberg.page\/articles/g' \
-        | sed 's/lupyuen.github.io\/images/lupyuen.codeberg.page\/images/g' \
-        | sed 's/lupyuen.github.io\/rss/lupyuen.codeberg.page\/rss/g' \
-        | sed 's/lupyuen.github.io\/sitemap/lupyuen.codeberg.page\/sitemap/g' \
-        | sed 's/lupyuen.github.io\/<\/loc>/lupyuen.codeberg.page\/<\/loc>/g' \
-        | sed 's/github.com\/lupyuen\/lupyuen.github.io\/blob\/master\/images/codeberg.org\/lupyuen\/pages\/src\/branch\/main\/images/g' \
-        >$html
+    if [[ "$PWD" == *"lupyuen.org"* ]]; then
+      # Rewrite to lupyuen.org
+      cat $tmp \
+          | sed 's/lupyuen.github.io\"/lupyuen.org\"/g' \
+          | sed 's/lupyuen.github.io\/articles/lupyuen.org\/articles/g' \
+          | sed 's/lupyuen.github.io\/images/lupyuen.org\/images/g' \
+          | sed 's/lupyuen.github.io\/rss/lupyuen.org\/rss/g' \
+          | sed 's/lupyuen.github.io\/sitemap/lupyuen.org\/sitemap/g' \
+          | sed 's/lupyuen.github.io\/<\/loc>/lupyuen.org\/<\/loc>/g' \
+          | sed 's/github.com\/lupyuen\/lupyuen.github.io\/blob\/master\/images/codeberg.org\/lupyuen\/pages\/src\/branch\/main\/images/g' \
+          >$html
+    else
+      # Rewrite to lupyuen.codeberg.page
+      cat $tmp \
+          | sed 's/lupyuen.github.io\"/lupyuen.codeberg.page\"/g' \
+          | sed 's/lupyuen.github.io\/articles/lupyuen.codeberg.page\/articles/g' \
+          | sed 's/lupyuen.github.io\/images/lupyuen.codeberg.page\/images/g' \
+          | sed 's/lupyuen.github.io\/rss/lupyuen.codeberg.page\/rss/g' \
+          | sed 's/lupyuen.github.io\/sitemap/lupyuen.codeberg.page\/sitemap/g' \
+          | sed 's/lupyuen.github.io\/<\/loc>/lupyuen.codeberg.page\/<\/loc>/g' \
+          | sed 's/github.com\/lupyuen\/lupyuen.github.io\/blob\/master\/images/codeberg.org\/lupyuen\/pages\/src\/branch\/main\/images/g' \
+          >$html
+    fi
     rm $tmp
 }
 
@@ -43,7 +57,7 @@ function sync_folder() {
     # Go to the sync folder
     pushd $sync
 
-    # Rewrite lupyuen.github.io to lupyuen.codeberg.page in articles/*.html
+    # Rewrite lupyuen.github.io in articles/*.html
     set +x  #  Disable Echo.
     for f in src/*.md
     do
@@ -55,43 +69,51 @@ function sync_folder() {
     done
     set -x  #  Echo all commands.
 
-    # Rewrite lupyuen.github.io to lupyuen.codeberg.page in resume
+    # Rewrite lupyuen.github.io in resume
     set +x  #  Disable Echo.
     cp index.html articles
     generate_article index
     cp articles/index.html .
     set -x  #  Echo all commands.
 
-    # Rewrite lupyuen.github.io to lupyuen.codeberg.page in RSS Feed
+    # Rewrite lupyuen.github.io in RSS Feed
     set +x  #  Disable Echo.
     cp rss.xml articles/rss.html
     generate_article rss
     mv articles/rss.html rss.xml
     set -x  #  Echo all commands.
 
-    # Rewrite lupyuen.github.io to lupyuen.codeberg.page in robots.txt
+    # Rewrite lupyuen.github.io in robots.txt
     set +x  #  Disable Echo.
     cp robots.txt articles/robots.txt.html
     generate_article robots.txt
     mv articles/robots.txt.html robots.txt
     set -x  #  Echo all commands.
 
-    # Rewrite lupyuen.github.io to lupyuen.codeberg.page in Sitemap
+    # Rewrite lupyuen.github.io in Sitemap
     set +x  #  Disable Echo.
     cp sitemap.xml articles/sitemap.xml.html
     generate_article sitemap.xml
     mv articles/sitemap.xml.html sitemap.xml
     set -x  #  Echo all commands.
 
-    # Testing: Rewrite lupyuen.github.io to lupyuen.codeberg.page
+    # Testing: Rewrite lupyuen.github.io
     # generate_article lte2 ; exit
 
-    # Commit the modified files
-    git pull
-    git status
-    git add .
-    git commit --all --message="Sync from lupyuen.github.io"
-    git push
+    # Commit the modified files to Docker or lupyuen.codeberg.page
+    if [[ "$sync" == *"lupyuen.org" ]]; then
+      local src=$HOME/lupyuen.org
+      local dest=lupyuen:/usr/local/apache2/htdocs
+      for filename in $src/*; do
+        docker cp $filename $dest
+      done
+    else
+      git pull
+      git status
+      git add .
+      git commit --all --message="Sync from lupyuen.github.io"
+      git push
+    fi
 
     # Return to the previous folder
     popd
@@ -100,6 +122,9 @@ function sync_folder() {
 # Update the current folder
 git pull
 
+# Sync to lupyuen.org
+sync_folder ../lupyuen.org
+
 # Sync to Codeberg Pages
 sync_folder ../lupyuen.codeberg.page
 
@@ -107,3 +132,31 @@ sync_folder ../lupyuen.codeberg.page
 if [[ $(($RANDOM % 8)) == 0 ]]; then
     sync_folder ../codeberg.lupyuen.github.io
 fi
+
+exit
+
+# Setup for lupyuen.org
+# Based on https://hub.docker.com/_/httpd
+docker run \
+  -dit \
+  --name lupyuen \
+  -p 3003:80 \
+  -v "$PWD":/usr/local/apache2/htdocs/ \
+  httpd:2.4-alpine
+
+## Copy HTTPD Config
+docker cp \
+  lupyuen:/usr/local/apache2/conf/httpd.conf \
+  .
+
+## List HTML Files
+docker exec \
+  lupyuen \
+  ls -la /usr/local/apache2/htdocs
+
+## Copy HTML Files
+src=$HOME/lupyuen.org
+dest=lupyuen:/usr/local/apache2/htdocs
+for filename in $src/*; do
+  docker cp $filename $dest
+done
