d3eff111642cc915d548ca166bb58030c6d2ec27
Lup Yuen Lee <luppy@appkaki.com>
2025-01-19 16:05:48 +0800

    Somehow GoAccess needs to be restarted once in a while

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -192,6 +192,8 @@ docker cp lupyuen:/usr/local/apache2/logs/access_log .
 function run { for (( ;; )); do; goaccess access_log -o report.html --log-format=COMBINED --real-time-html --geoip-database=GeoLite2-City.mmdb ; date; sleep 10; done; } ; run &
 open report.html
 for (( ;; )); do; docker cp lupyuen:/usr/local/apache2/logs/access_log .; date; sleep 10; done;
+## Somehow GoAccess needs to be restarted once in a while
+for (( ; ; )); do ; pkill goaccess ; date ; sleep 60 ; done
 
 ## Update the Canonical Header
 ## <link rel="canonical" href="https://lupyuen.org/articles/TODO.html" />
